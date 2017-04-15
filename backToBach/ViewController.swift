4236ac6c8dea6af5358548fec3412a77b8f67246
West Kraemer <WestKraemer@gmail.com>
2017-04-15 11:35:20 -0400

    finish adding code to create player and track song on slider.

--- backToBach/ViewController.swift
+++ backToBach/ViewController.swift
@@ -7,12 +7,86 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player = AVAudioPlayer()
+    
+    let audioPath = Bundle.main.path(forResource: "bach", ofType: "mp3")
+    
+    var timer = Timer()
+    
+    func updateScrubber() {
+        
+        scrubber.value = Float(player.currentTime)
+        
+    }
 
+    
+    @IBAction func play(_ sender: Any) {
+        
+        player.play()
+        
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.updateScrubber), userInfo: nil, repeats: true)
+    }
+    
+    @IBAction func volumeChanged(_ sender: Any) {
+        
+        player.volume = volumeSlider.value
+    }
+    
+    @IBOutlet var volumeSlider: UISlider!
+    
+    
+    @IBAction func scrubberMoved(_ sender: Any) {
+        
+        player.currentTime = TimeInterval(scrubber.value)
+        
+    }
+    
+    @IBOutlet var scrubber: UISlider!
+    
+    @IBAction func pause(_ sender: Any) {
+        
+        player.pause()
+        
+        timer.invalidate()
+    }
+    
+    @IBAction func stop(_ sender: Any) {
+        
+        timer.invalidate()
+        
+        player.pause()
+        
+        // Re-start player at 0. Could also use --> player.currentTime = 0
+        do {
+            
+            try player = AVAudioPlayer(contentsOf: URL(fileURLWithPath: audioPath!))
+            
+        } catch {
+            //process error
+        }
+
+        
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        do {
+            
+            try player = AVAudioPlayer(contentsOf: URL(fileURLWithPath: audioPath!))
+            scrubber.maximumValue = Float(player.duration)
+            
+        } catch {
+            //process error
+        }
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
